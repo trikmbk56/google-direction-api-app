1d489f00c4d691e82c7ec1d317b2967d88b8fadf
trikmbk56 <trikm.bk56@gmail.com>
2016-07-05 17:20:40 +0700

    use direction api

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
     @places = Place.all
-    @hash = Gmaps4rails.build_markers(@places) do |place, marker|
-      marker.lat place.latitude
-      marker.lng place.longitude
-    end
   end
 end
